e8544bc0bc293709600ad2e5db918bb737ea8979
AnilBulut <bulutanil@gmail.com>
2018-06-17 20:56:17 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 import java.io.File;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException {
-		Parser parser = new Parser();
-		//parser.parseCommand(args[0]);
-		parser.parseCommand("FileCrypt -e -p 4 -i document2.txt -o document2.encrypted AES CTR key_file.txt");
-		parser.openAndParseKeyFile(parser.getKeyFile());
+    public static void main(String[] args) throws InterruptedException {		
+		
+	Parser parser = new Parser();
+        parser.parseCommand("FileCrypt -e -p 4 -i document2.txt -o document2.encrypted AES CTR key_file.txt");
+	parser.openAndParseKeyFile(parser.getKeyFile());
         
         File inputFile = new File(parser.getInputFile());
         File outputFile = new File(parser.getOutputFile());
@@ -16,6 +16,7 @@ public class Main {
         
         AbstractCryptoFactory acf=AbstractCryptoFactory.getCryptoFactory(parser.getAlgorithm()).getCryptor(parser.getMode());
         acf.setNumberOfThreads(parser.getNumberOfThread());
+		
         if(parser.getType().equals("encryption")){
         	startTime = System.nanoTime();
         	acf.encrypt(parser.getKey(), parser.getVector(), inputFile, outputFile);
@@ -30,6 +31,7 @@ public class Main {
         	duration = (endTime - startTime)/1000000;
         	System.out.println(parser.getType()+" time: "+duration+" milliseconds");
         }
+		
         Log log = new Log();
         log.createLog(parser.getInputFile(), parser.getOutputFile(), parser.getType(), parser.getAlgorithm(), parser.getMode(), duration);
 		
